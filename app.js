f0fe1cafb80a85e1c7795bdb2cb6a4b4893c4e3f
hieudevhouse <nguyenminhhieu2003hd@gmail.com>
2026-05-04 21:11:50 +0700

    fix 33

--- app.js
+++ app.js
@@ -36,4 +36,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+}
+
+module.exports = app;
